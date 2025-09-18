73b4e1f8b75a0d5186dd4140a6a5435c7e8b6219
Mark-stack-aft <syho2058@gmail.com>
2025-09-18 14:47:50 +0900

    python

--- 02.변수, 연산, 입출력/test/test02.py
+++ 02.변수, 연산, 입출력/test/test02.py
@@ -1,9 +1,9 @@
-width = int(input("사각형의 너비는? "))
-height = int(input("사각형의 높이는? "))
-length = 2 * width + 2 * height
-area = width * height
+width = int(input("사각형의 너비는? ")) #입력값 받기
+height = int(input("사각형의 높이는? ")) #입력값 받기
+length = 2 * width + 2 * height #둘레 길이 구하기
+area = width * height #너비 * 높이를 하여 면적 구하기
 
-print("사각형의 너비 : %dcm" % width)
-print("사각형의 높이 : %dcm" % height)
-print("둘레 길이 : %dcm" % length)
-print("면적 : %dcm2" % area)
+print("사각형의 너비 : %dcm" % width) #사각형 너비 출력하기
+print("사각형의 높이 : %dcm" % height) #사각형 높이 출력하기
+print("둘레 길이 : %dcm" % length) #둘레 길이 출력하기
+print("면적 : %dcm2" % area) #연적 출력하기
