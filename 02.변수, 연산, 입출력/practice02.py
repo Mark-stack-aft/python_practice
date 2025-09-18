c3a344c98ce869aac549cd2e8d0a572969bbc7ba
Mark-stack-aft <syho2058@gmail.com>
2025-09-18 14:03:47 +0900

    d

---
+++ 02.변수, 연산, 입출력/practice02.py
@@ -0,0 +1,19 @@
+a = 10
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+
+d = 2**4 + 5%3
+print(d)
+
