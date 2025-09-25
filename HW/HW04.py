babcb14f8acc44a24498749e65a71e0629463855
Mark-stack-aft <syho2058@gmail.com>
2025-09-25 19:00:10 +0900

    py + test + HW

---
+++ HW/HW04.py
@@ -0,0 +1,27 @@
+print('\n' * 28)
+print('-' * 50)
+print()
+num = int(input('숫자를 입력하세요 : '))
+print()
+print('-' * 50)
+
+if num > 10:
+    print()
+    print(f'{num}은(는) 10보다 크다.')
+    print()
+    print('-' * 50)
+    print()
+
+elif num < 10:
+    print()
+    print(f'{num}은(는) 10보다 작다.')
+    print()
+    print('-' * 50)
+    print()
+
+else:
+    print()
+    print(f'{num}은(는) 10과 같다.')
+    print()
+    print('-' * 50)
+    print()
