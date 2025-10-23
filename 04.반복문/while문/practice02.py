0ad9b78c539b59f860e597aacf56b8fa1f0026f5
Mark-stack-aft <syho2058@gmail.com>
2025-10-23 13:56:24 +0900

    py

---
+++ 04.반복문/while문/practice02.py
@@ -0,0 +1,15 @@
+sum = 0
+num = 1
+
+while num <= 10:
+    sum += num      # sum = sum + num
+    print(f'값 : {sum}')
+    num += 1    # num = num + 1
+
+#####################################################
+
+s = 0
+
+for num in range(1, 11):
+    s += num
+    print(f'값 : {s}')
