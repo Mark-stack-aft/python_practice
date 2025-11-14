88512839b58e0c63ed6f21ac7a113620f9a6f46a
Mark-stack-aft <syho2058@gmail.com>
2025-11-14 17:27:28 +0900

    upgrade

--- 06.딕셔너리/dic.py
+++ 06.딕셔너리/dic.py
@@ -1,55 +1,318 @@
 import json
 from dict_mod import *
 from traceback import format_exc
+import keyboard
+from orign_dic import orign_dic
+import colorama
+from colorama import Fore, Back, Style
 
-with open(r'06.딕셔너리/dic.json', 'r') as file:
-    dic = json.load(file)
+def make():
+    with open(r'06.딕셔너리/person.json', 'r') as file:
+        persons = json.load(file)
 
+    print()
+    print('---회원가입---')
+    
+    try:
+        et = True
 
-# dic = {}
+        while et:
+            new_id = input('아이디 : ')
+            
+            if len(new_id) >= 10:
+                et = False
+            else:
+                print('아이디의 길이는 10자 이상이여야 합니다.')
+                et = True
+            
+            if new_id in persons.keys():
+                print('이미 있는 아이디 입니다.')
+                et = True
+            else:
+                et = False
 
-clear_screen()
+        et = True
 
-try:
-    while True:
-        func = input('기능을 선택하세요 (0 : 종료, 1 : 추가, 2 : 삭제, 3 : 수정, 4 : 검색, 5 : 키워드 검색, 000 : 전부 삭제) : ')
+        while et:
+            new_password = input('비밀번호 : ')
+            
+            if len(new_id) >= 10:
+                et = False
+            else:
+                print('비밀번호의 길이는 10자 이상이여야 합니다.')
+                et = True
 
-        if func == '0':
-            break
+        persons[new_id] = new_password
 
-        elif func == '1':
-            dic = plus(dic)
+        with open(r'06.딕셔너리/person.json', 'w') as file:
+            persons = json.dump(persons, file, indent = 2)
 
-        elif func == '2':
-            dic = delete(dic)
+        with open(r'06.딕셔너리/dic.json', 'w') as file:
+            dic_ = json.load(file)
+            dic_[persons] = orign_dic
+            json.dump(dic_, file, indent = 2)
 
-        elif func == '3':
-            dic = correct(dic)
+    except KeyboardInterrupt:
+        print('\n회원가입이 취소되었습니다.')
 
-        elif func == '000':
-            dic = dic_clear(dic)
-                    
-        elif func == '4':
+def login():
+    with open(r'06.딕셔너리/person.json', 'r') as file:
+        persons = json.load(file)
 
-            find_wd = input('단어나 뜻을 입력하세요 : ')
+    person = ''
 
-            func, dic = find(find_wd, dic)
+    print()
 
-        elif func == '5':
+    id = input('아이디 입력 : ')
+    password = input('비밀번호 입력 : ')
 
-            key_word = input('키워드를 입력하세요 : ')
-
-            func, dic = find_kw(key_word, dic)
+    if id in persons.keys():
+        if password == persons[id]:
+            print('로그인 성공!')
+            person = id
 
         else:
-            print('잘못 입력하였습니다.')
+            print('아이디나 비밀번호가 틀렸습니다.')
 
-except KeyboardInterrupt:
-    print('\n종료합니다.')
+    else:
+        print('회원가입을 하시겠습니까?')
+        print('   네    ⚪ 아니오', end = '', flush = True)
 
-except:
-    print('\n오류가 발생했습니다. | 오류 : %s' % format_exc().split('\n')[-2])
+        click = False
 
-finally:
-    with open(r'06.딕셔너리/dic.json', 'w') as file:
-        json.dump(dic, file, indent = 2)
+        while True:
+        
+            event = False
+
+            keyboard.read_key(suppress=True)
+
+            event = keyboard.read_event(suppress=True)
+
+            if event.name == 'enter' or event.name == 'return':
+                if click == True:
+                    make()
+                            
+                else:
+                    print('\n취소되었습니다.')
+
+                break
+
+            elif event.name == 'left' or event.name == 'right':
+                if click == True:
+                    click = False
+                    print('\r   네  ⚪ 아니오', end = '')
+
+                else:
+                    click = True
+                    print('\r⚪ 네     아니오', end = '')
+
+    return person
+
+def diction(person, color):
+    with open(r'06.딕셔너리/dic.json', 'r') as file:
+            dic_ = json.load(file)
+            dic = dic_[person]
+    try:
+        if color == True:
+            # dic = {}
+
+            clear_screen()
+
+            try:
+                while True:
+                    func = input(Back.WHITE + Style.DIM + '기능을 선택하세요 (0 : 종료, 1 : 추가, 2 : 삭제, 3 : 수정, 4 : 검색, 5 : 키워드 검색, 6 : 번역, 000 : 전부 삭제) : ' + Style.RESET_ALL)
+
+                    if func == '0':
+                        break
+
+                    elif func == '1':
+                        dic = plus(dic, color)
+
+                    elif func == '2':
+                        dic = delete(dic, color)
+
+                    elif func == '3':
+                        dic = correct(dic, color)
+
+                    elif func == '000':
+                        dic = dic_clear(dic, color)
+                                
+                    elif func == '4':
+
+                        find_wd = input(Back.CYAN + '단어나 뜻을 입력하세요 : ' + Style.RESET_ALL)
+
+                        func, dic = find(find_wd, dic, color)
+
+                    elif func == '5':
+
+                        key_word = input(Back.BLACK + Fore.WHITE + '키워드를 입력하세요 : ' + Style.RESET_ALL)
+
+                        func, dic = find_kw(key_word, dic, color)
+
+                    elif func == '6':
+
+                        tr_str = input(Back.LIGHTBLACK_EX + Fore.LIGHTWHITE_EX + '번역할 것을 입력하세요 : ' + Style.RESET_ALL)
+                        trans(tr_str, color)
+
+
+                    else:
+                        print(Back.LIGHTYELLOW_EX + '잘못 입력하였습니다.' + Style.RESET_ALL)
+
+            except KeyboardInterrupt:
+                print(Back.WHITE + Style.DIM + '\n종료합니다.' + Style.RESET_ALL)
+
+            except:
+                print(Back.LIGHTYELLOW_EX + '\n오류가 발생했습니다. | 오류 : %s' % format_exc().split('\n')[-2] + Style.RESET_ALL)
+
+        else:
+            # dic = {}
+
+            clear_screen()
+
+            try:
+                while True:
+                    func = input('기능을 선택하세요 (0 : 종료, 1 : 추가, 2 : 삭제, 3 : 수정, 4 : 검색, 5 : 키워드 검색, 6 : 번역, 000 : 전부 삭제) : ')
+
+                    if func == '0':
+                        break
+
+                    elif func == '1':
+                        dic = plus(dic, color)
+
+                    elif func == '2':
+                        dic = delete(dic, color)
+
+                    elif func == '3':
+                        dic = correct(dic, color)
+
+                    elif func == '000':
+                        dic = dic_clear(dic, color)
+                                
+                    elif func == '4':
+
+                        find_wd = input('단어나 뜻을 입력하세요 : ')
+
+                        func, dic = find(find_wd, dic, color)
+
+                    elif func == '5':
+
+                        key_word = input('키워드를 입력하세요 : ')
+
+                        func, dic = find_kw(key_word, dic, color)
+
+                    elif func == '6':
+
+                        tr_str = input('번역할 것을 입력하세요 : ')
+                        trans(tr_str, color)
+
+
+                    else:
+                        print('잘못 입력하였습니다.')
+
+            except KeyboardInterrupt:
+                print('\n종료합니다.')
+
+            except:
+                print('\n오류가 발생했습니다. | 오류 : %s' % format_exc().split('\n')[-2])
+
+    finally:
+        if not person == '':
+            dic_[person] = dic
+
+            with open(r'06.딕셔너리/dic.json', 'w') as file:
+                json.dump(dic_, file, indent = 2)
+
+def dictionary(bool, person, color):
+    if bool:
+        diction(person, color)
+
+    else:
+        diction('', color)
+
+def main():
+
+    person = ''
+    color = False
+
+    print(
+'''
+-----------------------------------------
+|                  사전                 |
+-----------------------------------------
+''')
+    
+    print('   로그인    ⚪ 비회원', end = '', flush = True)
+
+    click = False
+
+    while True:
+        
+        event = False
+
+        keyboard.read_key(suppress=True)
+
+        event = keyboard.read_event(suppress=True)
+
+        if event.name == 'enter' or event.name == 'return':
+            if click == True:
+                person = login()
+
+                if person == '':
+                    continue
+
+                else:
+                    break
+
+        elif event.name == 'left' or event.name == 'right':
+            if click == True:
+                click = False
+                print('\r   로그인    ⚪ 비회원', end = '')
+
+            else:
+                click = True
+                print('\r⚪ 로그인       비회원', end = '')
+
+    print(
+'''
+-----------------------------------------
+|                  설정                 |
+-----------------------------------------
+''')
+    
+    print('   색 있음    ⚪ 색 없음', end = '', flush = True)
+
+    click = False
+
+    while True:
+        
+        event = False
+
+        keyboard.read_key(suppress=True)
+
+        event = keyboard.read_event(suppress=True)
+
+        if event.name == 'enter' or event.name == 'return':
+            if click == True:
+                color = True
+
+            else:
+                color = False
+
+            break
+
+        elif event.name == 'left' or event.name == 'right':
+            if click == True:
+                click = False
+                print('\r   색 있음    ⚪ 색 없음', end = '')
+
+            else:
+                click = True
+                print('\r⚪ 색 있음       색 없음', end = '')
+
+    if person == '':
+        dictionary(False, '', color)
+
+    else:
+        dictionary(True, person, color)
+    
+main()
--- 06.딕셔너리/dict_mod.py
+++ 06.딕셔너리/dict_mod.py
@@ -3,6 +3,20 @@ import keyboard
 from password import get_password
 import os
 from termcolor import colored
+import googletrans
+import socket
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+def check_internet(host="8.8.8.8", port=53, timeout=5):
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        return False
 
 def clear_screen():
     if os.name == 'nt':
@@ -11,50 +25,75 @@ def clear_screen():
     else:
         os.system('clear')
 
-def plus(dic):
-    key = input('추가할 단어를 입력하세요 : ')
+def auto_find(dic, auto_wd, color):
+    if check_internet():
+        plus_wd = trans(auto_wd)
 
-    if not key in dic.keys() and not key in dic.values():
-        value = input('뜻 : ')
-                
-        if langid.classify(key)[0] == 'en' and langid.classify(value)[0] == 'ko':
+        if not (langid.classify(auto_wd)[0] == 'ko' and langid.classify(plus_wd)[0] == 'ko') or not (langid.classify(auto_wd)[0] == 'en' and langid.classify(plus_wd)[0] == 'en'):
+            if langid.classify(auto_wd)[0] == 'en' and langid.classify(plus_wd)[0] == 'ko':
+                dic[auto_wd] = plus_wd
+
+            elif langid.classify(auto_wd)[0] == 'ko' and langid.classify(plus_wd)[0] == 'en':
+                dic[plus_wd] = auto_wd
+
+    else:
+        print(Back.LIGHTRED_EX + '인터넷이 연결 되어 있지 않음 -> 오토 추가 기능 사용 불가' + Style.RESET_ALL if color else '인터넷이 연결 되어 있지 않음 -> 오토 추가 기능 사용 불가')
+
+def plus(dic, color):
+    key = input(Back.LIGHTGREEN_EX + '추가할 단어를 입력하세요 : ' + Style.RESET_ALL if color else '추가할 단어를 입력하세요 : ')
+
+    if not key in dic.keys():
+        value = input(Back.LIGHTGREEN_EX + '뜻 : ' + Style.RESET_ALL if color else '뜻 : ')
+        
+        if not value in dic.keys():
+            if langid.classify(key)[0] == 'en' and langid.classify(value)[0] == 'ko':
                 dic[key] = value
 
-        elif langid.classify(key)[0] == 'ko' and langid.classify(value)[0] == 'en':
-            dic[value] = key
+            elif langid.classify(key)[0] == 'ko' and langid.classify(value)[0] == 'en':
+                dic[value] = key
+
+            else:
+                print(Back.LIGHTYELLOW_EX + '영/한 구조가 아닙니다!' + Style.RESET_ALL if color else '영/한 구조가 아닙니다!')
 
         else:
-            print('영/한 구조가 아닙니다!')
+            print(Back.LIGHTYELLOW_EX + '단어가 이미 있습니다!' + Style.RESET_ALL if color else '단어가 이미 있습니다!')
+    
     else:
-        print('단어가 이미 있습니다!')
+        print(Back.LIGHTYELLOW_EX + '단어가 이미 있습니다!' + Style.RESET_ALL if color else '단어가 이미 있습니다!')
 
     return dic
 
-def delete(dic):
-    key = input('삭제할 단어를 입력하세요 : ')
+def delete(dic, color):
+    key = input(Back.MAGENTA + '삭제할 단어를 입력하세요 : ' + Style.RESET_ALL  if color else '삭제할 단어를 입력하세요 : ')
 
     if langid.classify(key)[0] == 'en':
         if key in dic:
             dic.pop(key)
 
+        else:
+            print(Back.LIGHTYELLOW_EX + '오류! 삭제할 단어가 없습니다!' + Style.RESET_ALL if color else '오류! 삭제할 단어가 없습니다!')
+
     elif langid.classify(key)[0] == 'ko':
         if key in dic.values():
             dic.pop(list(dic.keys())[list(dic.values()).index(key)])
 
+        else:
+            print(Back.LIGHTYELLOW_EX + '오류! 삭제할 단어가 없습니다!' + Style.RESET_ALL if color else '오류! 삭제할 단어가 없습니다!')
+
     else:
-        print('오류! 삭제할 단어가 없습니다!')
+        print(Back.LIGHTYELLOW_EX + '오류! 삭제할 단어가 없습니다!' + Style.RESET_ALL if color else '오류! 삭제할 단어가 없습니다!')
 
     return dic
 
-def correct(dic):
-    key = input('수정할 단어를 입력하세요 : ')
+def correct(dic, color):
+    key = input(Back.LIGHTBLUE_EX + '수정할 단어를 입력하세요 : ' + Style.RESET_ALL if color else '수정할 단어를 입력하세요 : ')
 
     if langid.classify(key)[0] == 'en':
         if key in dic:
-            re_key = input('수정할 단어의 수정된 단어를 입력하세요 : ')
-            re_value = input('수정할 단어의 수정된 뜻을 입력하세요 : ')
+            re_key = input(Back.LIGHTBLUE_EX + '수정할 단어의 수정된 단어를 입력하세요 : ' + Style.RESET_ALL if color else '수정할 단어의 수정된 단어를 입력하세요 : ')
+            re_value = input(Back.LIGHTBLUE_EX + '수정할 단어의 수정된 뜻을 입력하세요 : ' + Style.RESET_ALL if color else '수정할 단어의 수정된 뜻을 입력하세요 : ')
 
-            dic.delete(key)
+            dic.pop(key)
 
             if langid.classify(re_key)[0] == 'en' and langid.classify(re_value)[0] == 'ko':
                 dic[re_key] = re_value
@@ -63,12 +102,12 @@ def correct(dic):
                 dic[re_value] = re_key
 
             else:
-                print('영/한 구조가 아닙니다!')
+                print(Back.LIGHTYELLOW_EX + '영/한 구조가 아닙니다!' + Style.RESET_ALL if color else '영/한 구조가 아닙니다!')
 
     elif langid.classify(key)[0] == 'ko':
         if key in dic.values():
-            re_key = input('수정할 단어의 수정된 단어를 입력하세요 : ')
-            re_value = input('수정할 단어의 수정된 뜻을 입력하세요 : ')
+            re_key = input(Back.LIGHTBLUE_EX + '수정할 단어의 수정된 단어를 입력하세요 : ' + Style.RESET_ALL if color else '수정할 단어의 수정된 단어를 입력하세요 : ')
+            re_value = input(Back.LIGHTBLUE_EX + '수정할 단어의 수정된 뜻을 입력하세요 : ' + Style.RESET_ALL if color else '수정할 단어의 수정된 뜻을 입력하세요 : ')
 
             dic.pop(list(dic.keys())[list(dic.values()).index(key)])
 
@@ -79,16 +118,16 @@ def correct(dic):
                 dic[re_value] = re_key
 
             else:
-                print('영/한 구조가 아닙니다!')
+                print(Back.LIGHTYELLOW_EX + '영/한 구조가 아닙니다!' + Style.RESET_ALL if color else '영/한 구조가 아닙니다!')
 
     else:
-        print('오류! 수정할 단어가 없습니다!')
+        print(Back.LIGHTYELLOW_EX + '오류! 수정할 단어가 없습니다!' + Style.RESET_ALL if color else '오류! 수정할 단어가 없습니다!')
 
     return dic
 
-def dic_clear(dic):
-    print('정말로 삭제하시겠습니까? : ')
-    print(' 네    ⚪ 아니오', end = '', flush = True)
+def dic_clear(dic, color):
+    print(Back.RED + '정말로 삭제하시겠습니까? : ' + Style.RESET_ALL if color else '정말로 삭제하시겠습니까? : ')
+    print(Back.RED + '   네    ⚪ 아니오'  + Style.RESET_ALL if color else '   네    ⚪ 아니오', end = '', flush = True)
 
     click = False
 
@@ -103,92 +142,125 @@ def dic_clear(dic):
         if event.name == 'enter' or event.name == 'return':
             if click == True:
 
-                password = input('\n비밀번호 : ')
+                password = input(Back.RED + '\n비밀번호 : ' + Style.RESET_ALL if color else '\n비밀번호 : ')
 
                 if password == get_password:
                     dic.clear()
-                    print('\n완료되었습니다.')
+                    print(Back.RED + '\n완료되었습니다.' + Style.RESET_ALL if color else '\n완료되었습니다.')
 
                 else:
-                    print('\n비밀번호가 틀렸습니다.')
+                    print(Back.RED + '\n비밀번호가 틀렸습니다.' + Style.RESET_ALL if color else '\n비밀번호가 틀렸습니다.')
                         
             else:
-                print('\n취소되었습니다.')
+                print(Back.RED + '\n취소되었습니다.' + Style.RESET_ALL if color else '\n취소되었습니다.')
 
             break
 
         elif event.name == 'left' or event.name == 'right':
             if click == True:
                 click = False
-                print('\r   네  ⚪ 아니오', end = '')
+                print(Back.RED + '\r   네  ⚪ 아니오      ' + Style.RESET_ALL if color else '\r   네  ⚪ 아니오      ', end = '')
 
             else:
                 click = True
-                print('\r⚪ 네     아니오', end = '')
-
-        keyboard.read_key(suppress=True)
+                print(Back.RED + '\r⚪ 네     아니오      ' + Style.RESET_ALL if color else '\r⚪ 네     아니오      ', end = '')
 
     return dic
 
-def find(find_wd, dic):
+def find(find_wd, dic, color):
 
     key_value = find_wd
 
     if langid.classify(key_value)[0] == 'en':
         if key_value in dic.keys():
-            print(f'단어 : {key_value}, 뜻 : {dic[key_value]}')
+            print(Back.CYAN + f'단어 : {key_value}, 뜻 : {dic[key_value]}' + Style.RESET_ALL if color else f'단어 : {key_value}, 뜻 : {dic[key_value]}')
 
         else:
-            print('단어가 없습니다!')
+            print(Back.LIGHTYELLOW_EX + '단어가 없습니다!' + Style.RESET_ALL if color else '단어가 없습니다!')
+            auto_find(dic, key_value)
 
     elif langid.classify(key_value)[0] == 'ko':
-        if key_value in dic.values():
-            print(f'단어 : {list(dic.keys())[list(dic.values()).index(key_value)]}, 뜻 : {key_value}')
 
+        found_list_wd, found_list_def = [], []
+        num = 0
+
+        if key_value in dic.values():
+            
+            for indx in range(len(dic)):
+                if key_value in list(dic.values())[indx].split(','):
+                    found_list_def.append(list(dic.values())[indx])
+                    found_list_wd.append(list(dic.keys())[indx])
+                    num = num + 1
+
+            if num > 0:
+                for printing in range(num):
+                    print(Back.CYAN + f'단어 : {found_list_wd[printing]}, 뜻 : {found_list_def[printing]}' + Style.RESET_ALL if color else f'단어 : {found_list_wd[printing]}, 뜻 : {found_list_def[printing]}')
+            
         else:
-            print('단어가 없습니다!')
+            print(Back.LIGHTYELLOW_EX + '단어가 없습니다!' + Style.RESET_ALL if color else '단어가 없습니다!')
+            auto_find(dic, key_value, color)
 
     return find_wd, dic
 
-def find_kw(key_word, dic):
+def find_kw(key_word, dic, color):
     key_value = key_word
-
-    found_list = []
-
+    found_list_wd, found_list_def = [], []
     attrs = ['bold']
+    num = 0
 
     if langid.classify(key_value)[0] == 'en':
-        for key in list(dic.keys()):
-            if key_value in key:
-                found_list.append(key.replace(key_value, '⬛'))
+        for indx in range(len(dic)):
+            if key_value in list(dic.keys())[indx]:
+                found_list_wd.append(list(dic.keys())[indx].replace(key_value, '⬛'))
+                found_list_def.append(list(dic.values())[indx])
+                num = num + 1
 
-        if bool(found_list) == True:
-            for printing in found_list:
-                print('단어 : ', end = '')
+        if num > 0:
+            for printing in range(num):
+                print(Back.BLACK + Fore.WHITE + '단어 : '  + Style.RESET_ALL if color else '단어 : ', end = '')
                 
-                for ind in printing:
-                    print(colored(key_value, 'red', attrs=['bold']) if ind == '⬛' else ind, end = '')
+                for ind in found_list_wd[printing]:
+                    print(Back.BLACK + colored(key_value, 'red', attrs=['bold']) if ind == '⬛' else Fore.WHITE + ind if color else colored(key_value, 'red', attrs=['bold']) if ind == '⬛' else ind, end = '')
+                    print(Style.RESET_ALL if color else '', end = '')
 
-                print(f', 뜻 : {dic[printing.replace('⬛', key_value)]}')
+                print(Back.BLACK + Fore.WHITE + f', 뜻 : {found_list_def[printing]}' + Style.RESET_ALL if color else f', 뜻 : {found_list_def[printing]}')
 
         else:
-            print('단어가 없습니다!')
+            print(Back.LIGHTYELLOW_EX + '단어가 없습니다!' + Style.RESET_ALL if color else '단어가 없습니다!')
 
     elif langid.classify(key_value)[0] == 'ko':
-        for value in list(dic.values()):
-            if key_value in value:
-                found_list.append(value.replace(key_value, '⬛'))
+        for indx in range(len(dic)):
+            if key_value in list(dic.values())[indx]:
+                found_list_def.append(list(dic.values())[indx].replace(key_value, '⬛'))
+                found_list_wd.append(list(dic.keys())[indx])
+                num = num + 1
 
-        if bool(found_list) == True:
-            for printing in found_list:
-                print(f'단어 : {list(dic.keys())[list(dic.values()).index(printing.replace('⬛', key_value))]}, 뜻 : ', end = '')
+        if num > 0:
+            for printing in range(num):
+                print(Back.BLACK + Fore.WHITE + f'단어 : {found_list_wd[printing]}, 뜻 : ' + Style.RESET_ALL if color else f'단어 : {found_list_wd[printing]}, 뜻 : ', end = '')
                 
-                for ind in printing:
-                    print(colored(key_value, 'red', attrs=['bold']) if ind == '⬛' else ind, end = '')
+                for ind in found_list_def[printing]:
+                    print(Back.BLACK + colored(key_value, 'red', attrs=['bold']) if ind == '⬛' else Fore.WHITE + ind if color else colored(key_value, 'red', attrs=['bold']) if ind == '⬛' else ind, end = '')
+                    print(Style.RESET_ALL if color else '', end = '')
 
                 print()
 
         else:
-            print('단어가 없습니다!')
+            print(Back.LIGHTYELLOW_EX + '단어가 없습니다!' + Style.RESET_ALL if color else '단어가 없습니다!')
 
     return key_value, dic
+
+def trans(in_str, color):
+    if check_internet():
+        if langid.classify(in_str)[0] == 'ko':
+            out_str = googletrans.Translator().translate(in_str, dest = 'en', src = 'auto')
+
+        elif langid.classify(in_str)[0] == 'en':
+            out_str = googletrans.Translator().translate(in_str, dest = 'ko', src = 'auto')
+
+        print(Back.LIGHTBLACK_EX + Fore.LIGHTWHITE_EX + f'{in_str} : 번역됨 -> {out_str.text}' + Style.RESET_ALL if color else f'{in_str} : 번역됨 -> {out_str.text}')
+
+        return out_str.text
+    
+    else:
+        print(Back.LIGHTRED_EX + '인터넷 연결이 되어있지 않아서, 번역이 불가능 합니다.' + Style.RESET_ALL if color else '인터넷 연결이 되어있지 않아서, 번역이 불가능 합니다.')
