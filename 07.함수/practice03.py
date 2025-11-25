16322ee912e18811fd97a5471166afc3b64616b6
SYH <syho2058@gmail.com>
2025-11-25 14:50:54 +0900

    py

---
+++ 07.함수/practice03.py
@@ -0,0 +1,26 @@
+def plus(num1, num2):
+    sum = num1 + num2
+
+    return sum
+
+def minus(num1, num2):
+    man = num1 - num2
+
+    return man
+
+print(plus(6, 2))
+print(minus(6, 8))
+
+def func1(num):
+    if num % 2 == 0:
+        print('짝수')
+
+    else:
+        print('홀수')
+
+func1(5)
+
+def func2(lis):
+    print(lis)
+
+func2(['1', '2', '3'])
---
+++ 07.함수/practice04.py
@@ -0,0 +1,5 @@
+from practice03 import func2
+import practice03
+
+func2([1, 2, 3, 4])
+practice03.func2([1, 2, 3, 4])
